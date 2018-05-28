f991fb27643fa37a7b2a6c8ea41ce5e21a9dc469
Monarch <monarchwadia@gmail.com>
2018-05-28 14:10:11 -0400

    Stage 3 - Added props to CountdownClock

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # React 101
 
+## Stage 3 - Passing Parameters
+
+One of React's strengths is the ability to parcel and reuse the same code in multiple places. In this stage, we're going to pass "props" to CountdownClock so we can configure the date we're counting down to.
+
+What does 'CountdownClock''s `render()` method do now? How are we passing properties into CountdownClock?
+
+If you wanted to render a second clock that counted down to New Year 2019, how would you do so?
+
+The next step makes the clock update automatically, like a real countdown timer should.
+
 ## Stage 2 - Our first component
 
 We have now added a simple message indicating how much time is left until some date in the future. This element is contained in `components/CountdownClock.js`.
--- countdown-timer/src/components/CountdownClock.js
+++ countdown-timer/src/components/CountdownClock.js
@@ -1,23 +1,36 @@
-// React is a required import for all files that use React's syntax (i.e., JSX syntax)
-// Component is a base React class that is extended to create custom components
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// Here, we create the CountdownClock component, which is imported and used in App.js
 class CountdownClock extends Component {
 
-  // Anything returned by render() is printed to the DOM. 
-  // render() should do nothing except form an output and return it.
-  // It is intended to be what is called a "pure" function.
   render() {
+    // save the props in variables, for clarity
+    const date = this.props.date;
+    const label = this.props.label;
 
+    // calculate how much time is left
+    const now = new Date();
+    const timeLeftMs = date - now;
+
+    // render the results
     return (
-      // React uses 'className' because 'class' is a reserved keyword in JavaScript
       <div className="countdown-clock">
-        0 days, 0 hours, 0 minutes, 0 seconds
+        { label }
+        <br/>
+        { timeLeftMs }
       </div>
     );
-
   }
+}
+
+// PropTypes is a standard React library that helps make your code safer
+// by making sure your props are as expected.
+// The Workhaus Academy React course dives deep into PropTypes
+// and other best practices, so that you can become a better 
+// developer and write cleaner, safer, better code.
+CountdownClock.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+  color: PropTypes.string.isRequired
 }
 
 export default CountdownClock;
